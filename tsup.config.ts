aee11ec7d0a01565540e2c3c7b0284c952f6a6ac
maag0708 <aamaro@ithub.com.mx>
2026-02-21 17:47:35 -0800

    build: update tsup configuration and remove previous build output.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  format: ["cjs", "esm"],
-  entry: ["./src/index.ts"],
+  entry: ["src/index.ts"],
+  format: ["esm", "cjs"],
   dts: true,
-  shims: true,
-  skipNodeModulesBundle: true,
+  splitting: false,
+  sourcemap: true,
   clean: true,
+  external: ["react", "react-dom"]
 });
